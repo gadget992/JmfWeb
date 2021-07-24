d3f92e3292a6d2793be1d0308482a7f38a611990
gadget992 <68819933+gadget992@users.noreply.github.com>
2021-07-24 10:09:11 -0300

    change psw

--- JmfWeb/JmfWeb/settings.py
+++ JmfWeb/JmfWeb/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "fernandezdavid1992@gmail.com"
-EMAIL_HOST_PASSWORD = "D020319f"
+EMAIL_HOST_USER = "#"
+EMAIL_HOST_PASSWORD = "#"
